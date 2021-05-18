286e1cb7b8887e68bf4deb711062b916f9673e45
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-18 09:52:53 -0400

    tests pass; probably could be simpler

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -9,6 +11,25 @@ package module01
 //   DecToBase(14, 16) => "E"
 //   DecToBase(14, 2) => "1110"
 //
+
 func DecToBase(dec, base int) string {
-	return ""
+	letterNumbers := map[string]string{
+		"10": "A",
+		"11": "B",
+		"12": "C",
+		"13": "D",
+		"14": "E",
+		"15": "F",
+	}
+
+	var result string
+	for dec != 0 {
+		remainder := fmt.Sprint(dec % base)
+		if letter, ok := letterNumbers[remainder]; ok {
+			remainder = letter
+		}
+		dec = dec / base
+		result = fmt.Sprint(remainder) + result
+	}
+	return result
 }
