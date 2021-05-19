09ec38f944a6721bbb240eb6ea1d7ba3edd86ffe
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-19 11:43:28 -0400

    tests pass for 0-9 numbers, not cleanest code

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,11 @@
 package module01
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +15,23 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	fmt.Printf("value: %v, base: %v\n", value, base)
+	var output int
+	valueStr := string(value)
+	for i := 0; i < len(valueStr); i++ {
+		x, _ := strconv.Atoi(string(valueStr[i]))
+		reversedPosition := len(valueStr) - i - 1
+		multiplier := math.Pow(float64(base), float64(reversedPosition))
+		converted := float64(x) * multiplier
+		fmt.Println(i, base, x, reversedPosition, multiplier, converted)
+		output += int(converted)
+	}
+	return output
 }
+
+/*
+	1  	2	1
+	10 	2   	2
+	21	3	7
+	1110	2	14
+*/
